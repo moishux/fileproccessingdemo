ff5f5528a3f4175e792dbcc2c23b63aec4edce68
marionravensapon <marionsapon.02@gmail.com>
2025-11-04 08:21:46 +0800

    Add project files.

---
+++ fileproccessingdemo/Form1.vb
@@ -0,0 +1,20 @@
+﻿
+Imports System.IO
+
+Public Class Form1
+    Private Sub ButtonWrite_Click(sender As Object, e As EventArgs) Handles ButtonWrite.Click
+        Try
+            Dim filePath As String = "sample.txt" ' File Location
+
+            Using writer As New StreamWriter(filePath)
+                writer.WriteLine("Hello, this is a test file!")
+                writer.WriteLine("Second line of the text.")
+            End Using
+
+            MessageBox.Show("Data written successfully.", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+End Class
---
+++ fileproccessingdemo/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("fileproccessingdemo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("fileproccessingdemo")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dd345fc4-44ef-455d-84ec-17e19deb72f8")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
